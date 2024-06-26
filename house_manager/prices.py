1080236ca95f16862124abffb81466bd6c528a67
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-06-26 09:48:55 +0100

    Update prices.py

--- house_manager/prices.py
+++ house_manager/prices.py
@@ -8,8 +8,12 @@ def get_electricity_price(dt: datetime) -> float:
     localtime = dt.astimezone(london)
     cheap_rate = (localtime.hour == 0 and localtime.hour >= 30) \
         or localtime.hour in (1, 2, 3) \
-        or (localtime.hour == 4 and localtime.hour < 30)
+        or ((localtime.hour == 4 or ((localtime.hour == 5 and localtime.hour < 30))
+            if dt >= datetime(2024, 7, 1) else
+            (localtime.hour == 4 and localtime.hour < 30))
 
+    if dt >= datetime(2024, 7, 1):
+        return 0.085 if cheap_rate else 0.24393
     if dt >= datetime(2024, 4, 1):
         return 0.09 if cheap_rate else 0.27936
     return 0.09 if cheap_rate else 0.3103
@@ -22,6 +26,8 @@ def get_electricity_standing_charge(dt: datetime) -> float:
 
 
 def get_gas_price(dt: datetime) -> float:
+    if dt >= datetime(2024, 7, 1):
+        return 0.05402
     if dt >= datetime(2024, 4, 1):
         return 0.05963
     return 0.0731
