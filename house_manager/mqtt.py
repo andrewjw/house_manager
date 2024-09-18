43e72507ff6454d352ba4dbe965511c511222835
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-09-18 22:09:53 +0100

    Remove foxess mqtt for now.

--- house_manager/mqtt.py
+++ house_manager/mqtt.py
@@ -6,7 +6,7 @@ import sys
 import paho.mqtt.client as mqtt
 from sentry_sdk import capture_exception
 
-from .foxess_msg import foxess_msg
+#from .foxess_msg import foxess_msg
 from .glow_msg import glow_msg
 
 
@@ -14,15 +14,15 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     client.subscribe("glow/+/+/+")
-    client.subscribe("foxess/+")
+    #client.subscribe("foxess/+")
 
 
 def safe_on_message(args, client, userdata, msg):
     try:
         if msg.topic.startswith("glow/"):
             return glow_msg(client, userdata, msg)
-        elif msg.topic.startswith("foxess/"):
-            return foxess_msg(args.foxess, msg)
+        #elif msg.topic.startswith("foxess/"):
+        #    return foxess_msg(args.foxess, msg)
         else:
             raise ValueError(f"Unknown topic {msg.topic}")
     except Exception as e:
