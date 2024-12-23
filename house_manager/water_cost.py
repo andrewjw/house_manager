e7a033e8c9725488ee19acba9a80a3c42fb5ee2e
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-12-23 21:58:49 +0000

    Handle exceptions querying prometheus for water usage.

--- house_manager/water_cost.py
+++ house_manager/water_cost.py
@@ -3,11 +3,14 @@ from datetime import datetime, UTC
 from time import sleep
 from threading import Thread
 from typing import Optional
+import sys
 
 from prometheus_api_client import PrometheusConnect  # type:ignore
+import requests
 
 from .prices import get_water_price, get_water_standing_charge
 from .utils import report_exception
+
 
 class WaterCost:
     def __init__(self) -> None:
@@ -27,14 +30,22 @@ class WaterCost:
     def _update(self) -> None:
         while True:
             dt = datetime.now(UTC)
-            data = self._prom.get_current_metric_value(metric_name='watermeter_count')
+            try:
+                data = self._prom.get_current_metric_value(
+                    metric_name='watermeter_count')
+            except requests.ConnectionError as e:
+                sys.stderr.write(f"Error querying water usage - {e}\n")
+                sys.stderr.flush()
+                data = []
+
             if len(data) == 0:
                 sleep(30)
                 continue
 
             usage = float(data[0]["value"][1])
 
-            if self.last_update is not None and self.last_update.date() != dt.date():
+            if self.last_update is not None \
+               and self.last_update.date() != dt.date():
                 self.cost += get_water_standing_charge(dt)
 
             self.last_update = dt
